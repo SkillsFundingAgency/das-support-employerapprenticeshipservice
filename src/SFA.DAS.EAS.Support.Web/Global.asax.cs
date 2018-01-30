67911cb133aec7d86a767165a7d71e83fb38518b
Iain.Stevenson <Iain.Stevenson@live.co.uk>
2018-01-30 17:49:27 +0000

    Wrapped HttpContext for exception handling

--- src/SFA.DAS.EAS.Support.Web/Global.asax.cs
+++ src/SFA.DAS.EAS.Support.Web/Global.asax.cs
@@ -17,7 +17,7 @@ namespace SFA.DAS.EAS.Support.Web
     {
         private void Application_Start(object sender, EventArgs e)
         {
-
+            //throw new Exception();
             //var ioc = DependencyResolver.Current;
             //var logger = ioc.GetService<ILog>();
             //logger.Info("Starting Web Role");
@@ -45,14 +45,59 @@ namespace SFA.DAS.EAS.Support.Web
             //    }
             //).Apply(new HttpContextWrapper(HttpContext.Current), PolicyConcern.HttpResponse);
         }
+        //protected void Application_Error(object sender, EventArgs e)
+        //{
+        //    var ex = Server.GetLastError().GetBaseException();
+        //    var logger = DependencyResolver.Current.GetService<ILog>();
+        //    var logger = new NLogLogger(typeof(HttpApplication));
+
+
+        //    logger.Error(ex, "App_Error");
+        //}
         protected void Application_Error(object sender, EventArgs e)
         {
             var ex = Server.GetLastError().GetBaseException();
-            //var logger = DependencyResolver.Current.GetService<ILog>();
-            var logger = new NLogLogger(typeof(HttpApplication));
+            BuildAndLogExceptionReport(ex);
+        }
+
+        private void BuildAndLogExceptionReport(Exception ex)
+        {
+
+            var logger =  new NLogLogger(typeof(HttpApplication));
+            ; // DependencyResolver.Current.GetService<ILog>();
+
+            var exceptionReport = $"An Unhandled exception was caught by {nameof(Application_Error)}\r\n";
+            exceptionReport += TryAddUserContext();
+            exceptionReport += TryAddHttpRequestContext();
+            exceptionReport += "\r\nException Stack Trace follows:\r\n\r\n";
+            logger.Error(ex, exceptionReport);
+
+        }
+
+        private string TryAddHttpRequestContext()
+        {
+            try
+            {
+                return
+                    $"Host Address: {HttpContext.Current?.Request?.UserHostAddress ?? "Unknown"}\r\nHttp Method: {HttpContext.Current?.Request?.HttpMethod ?? "Unknown"}\r\nRawUrl: {HttpContext.Current?.Request?.RawUrl ?? "Unknown"}";
+            }
+            catch (Exception e)
+            {
+                return "The HttpRequest is not available in this context.";
+            }
+        }
 
 
-            logger.Error(ex, "App_Error");
+        private string TryAddUserContext()
+        {
+            try
+            {
+                return $"User Name: {HttpContext.Current?.User?.Identity?.Name ?? "Unknown"}\r\n";
+            }
+            catch (Exception e)
+            {
+                return "The HttpRequest is not available in this context.";
+            }
         }
     }
 }
