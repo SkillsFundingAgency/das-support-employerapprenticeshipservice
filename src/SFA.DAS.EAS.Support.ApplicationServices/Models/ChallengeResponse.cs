5028237d958dfdf5fa8bb74bd9abeb226a4ef793
kola-samuel <samuel.dev.dba@gmail.com>
2018-03-06 15:54:52 +0000

    Return Challenege Result Object instead of empty string for valid challenge entry.

--- src/SFA.DAS.EAS.Support.ApplicationServices/Models/ChallengeResponse.cs
+++ src/SFA.DAS.EAS.Support.ApplicationServices/Models/ChallengeResponse.cs
@@ -6,10 +6,16 @@ namespace SFA.DAS.EAS.Support.ApplicationServices.Models
     [ExcludeFromCodeCoverage]
     public class ChallengeResponse
     {
+        public ChallengeResponse()
+        {
+            Characters = new List<int>();
+        }
+        
         public Core.Models.Account Account { get; set; }
 
         public List<int> Characters { get; set; }
 
         public SearchResponseCodes StatusCode { get; set; }
+
     }
 }
